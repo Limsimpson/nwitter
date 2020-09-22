699d411125f4d6b6436adb2fd4236421d28d127a
simpson <hisimpson90@gmail.com>
2020-09-22 11:58:45 +0900

    Updates

---
+++ precache-manifest.770e987113dff616fa8b65118ad0d001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1138f09b5fc9cd924514d8d6dcb46c45",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "c6b3e4da8b3b1cfb321b",
+    "url": "/nwitter/static/css/main.3f187fb6.chunk.css"
+  },
+  {
+    "revision": "450690a7fae6218f3252",
+    "url": "/nwitter/static/js/2.da8fdff2.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.da8fdff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b3e4da8b3b1cfb321b",
+    "url": "/nwitter/static/js/main.2cfab5bb.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
